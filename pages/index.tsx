5c4193edb2cef11f6cc63041ccc85ba585c570e5
mroenca40 <mproenca.marketing@gmail.com>
2022-04-12 18:52:01 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      <h1>Olá, Matheus</h1>
+    </div>
+  )
+}
